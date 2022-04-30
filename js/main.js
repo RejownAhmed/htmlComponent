12cfca8b20d62ad7c5201e7ea4f4ab6ee71b3708
Rejown <rejownahmed@yahoo.com>
2022-04-30 15:46:59 +0600

    sdf

---
+++ js/main.js
@@ -0,0 +1,36 @@
+$('.product_carousel').slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    lazyLoad: 'onprogress',
+    responsive: [
+      {
+        breakpoint: 1110,
+        settings: {
+          slidesToShow: 5,
+        }
+      },
+      {
+        breakpoint: 910,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 710,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 468,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
